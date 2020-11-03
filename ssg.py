9c1a8091ca4884eeb36c8cc725cd864e24d1dba3
rkarns <rkarns@cds-global.com>
2020-11-03 15:22:20 -0600

    Fix typo

--- ssg.py
+++ ssg.py
@@ -3,7 +3,7 @@ import typer
 from ssg.site import Site
 
 
-def main(source="content", dest="dest"):
+def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
 
     Site(**config).build()
